1953e37d8f5dde80a9f0e1381aa760c226984822
thealepo <sanchezc.alexe@gmail.com>
2026-02-10 13:15:22 -0500

    part 1

--- project1/main.c
+++ project1/main.c
@@ -9,15 +9,38 @@ char *args[MAX_LINE/2 + 1]; /* command line arguments */
 int should_run = 1; /* flag to determine when to exit program */
 
 while (should_run) {
-printf("osh&#x003E;");
-fflush(stdout);
+    printf("osh&#x003E;");
+    fflush(stdout);
 
-/**
-* After reading user input, the steps are:
-* (1) fork a child process using fork()
-* (2) the child process will invoke execvp()
-* (3) parent will invoke wait() unless command included &
-*/
-}
+    // ---------- PART 1 -------------
+    fgets(line , MAX_LINE , stdin);
+
+    char *token = strtok(line , " \n"); // splitting the line into tokens
+    int i = 0;
+    while (token != NULL){
+        args[i++] = token;
+        token = strtok(NULL , " ");
+    } // looping through the tokens until NULL, adding them to the args array
+    args[i] = NULL; // adding NULL to the end of the args array
+
+    // args -> [ps , ael , NULL]
+
+    pid_t pid = fork(); // forking a child process
+    if (pid == 0){
+        execvp(args[0] , args); // executing the command if it is a child process
+    }
+    else{
+        wait(NULL); // otherwise, wait for the child process to finish
+    }
+
+
+    /**
+    * After reading user input, the steps are:
+    * (1) fork a child process using fork()
+    * (2) the child process will invoke execvp()
+    * (3) parent will invoke wait() unless command included &
+    */
+    }
+
 return 0;
 }
